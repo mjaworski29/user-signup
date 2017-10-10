781a005b0a51683a302e885220ea46d553d4b911
Meredith Jaworski <mjaworski29@hotmail.com>
2017-10-09 19:39:09 -0500

    Finished project

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-<html>
-    <body>
-    <form action = "/signup" method = "post">
-        <label for="Username">Username</label>
-        <input id="Username" type="text" name = "Username" />
-        <input type ="submit" />
-        <label for="Password">Password </label>
-        <input id="Password" type="text" name="Password" />
-        <input type="submit" />
-        <label for="Verify Password"> Verify Password </label>
-        <input id="Verify Password" type = "text" name = "Verify Password />
-        <input type = "submit" />
-        <label for="Email"> Email (optional) </label>
-        <input id = "Email" type = "text" name = "Email" />
-        <input type = "submit" />
-    </form>
-    </body>
-</html>
-"""
 
 @app.route("/")
 def index():
-    return form
+    return render_template("user_signup_form.html")
 
-@app.route("/signup", methods = ["post"])
-def signup():
-    Username = request.form["Username"]
-    return "<h1>Hello," + Username + '</h1>'
+@app.route("/hello", methods=["POST"])
+def hello():
+    username = request.form["username"]
+    password = request.form["password"]
+    verify_password = request.form["verify_password"]
+    email = request.form["email"]
+
+    username_error = ""
+    password_error = ""
+    verify_password_error = ""
+    email_error = ""
+
+    is_error = False 
+
+    if username == "" or len(username) < 3 or len(username) > 20:
+        username_error = 'Please enter a valid Username'
+        is_error = True 
+
+    if password == "" or len(password) < 3 or len(password) > 20:
+        password_error = 'Please enter a valid Password'
+        is_error = True 
+
+    if verify_password == "" or verify_password != password:
+        verify_password_error = 'Please verify your Password'
+        is_error = True 
+
+    if is_error:
+       return render_template('user_signup_form.html', username_error=username_error, password_error=password_error, verify_password_error=verify_password_error, email_error=email_error)
+       
+    return render_template("hello.html", username=username)
+
 
 app.run()
